7b0b110fd8e399c11e0ec1f9d53c6e4c591f2e2a
Bryan Reed <bryan87reed@gmail.com>
2022-05-13 20:44:38 -0700

    Heroku init

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 //Get the default connection
 const db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
+//Bind connection to error event
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(
